8a62cab59fe043cb564c4282787266b0bb641524
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-08 01:02:04 +0900

    비밀정보관리

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/user");
+
+const config = require("./config/key");
+//바디파서 옵션
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://psh9408p:pksd1993@cluster0.t1xoq.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("mongoDB connected!!!!"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!asdasd");
+  res.send("nodemon hi!!!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port} asdsad`);
+  console.log(`Example app listening at http://localhost:${port} hihi`);
 });
