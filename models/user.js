6a627b4fa5267f1d337d9ac9aa3727aeab6361b5
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-08 17:05:02 +0900

    fix server page

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const jwt = require("jsonwebtoken");
 
-let userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     maxlength: 50,
@@ -11,6 +11,7 @@ let userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
+    unique: 1,
   },
   password: {
     type: String,
@@ -36,11 +37,11 @@ let userSchema = mongoose.Schema({
 userSchema.pre("save", function (next) {
   var user = this;
   if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
 
       bcrypt.hash(user.password, salt, function (err, hash) {
-        // Store hash in your password DB.
         if (err) return next(err);
         user.password = hash;
         next();
@@ -52,6 +53,7 @@ userSchema.pre("save", function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
@@ -60,15 +62,32 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
   var user = this;
+  // console.log('user._id', user._id)
 
+  // jsonwebtoken을 이용해서 token을 생성하기
   var token = jwt.sign(user._id.toHexString(), "secretToken");
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
 
-  user.toekn = token;
+  user.token = token;
   user.save(function (err, user) {
-    if (err) {
-      return cb(err);
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+  // user._id + ''  = token
+  //토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
       cb(null, user);
-    }
+    });
   });
 };
 
