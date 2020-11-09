6a53d7203f7e0d5148a423abb30c982260f662f1
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-10 00:18:16 +0900

    add logout

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
-const LandingPage = () => {
+const LandingPage = (props) => {
   useEffect(() => {
     getApi();
   }, []);
 
   const getApi = () => {
     Axios.get("/api/hello").then((response) => console.log(response.data));
+  };
+
+  const onClickHandler = () => {
+    Axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃실패");
+      }
+    });
   };
 
   return (
@@ -20,6 +30,7 @@ const LandingPage = () => {
       }}
     >
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>LogOut</button>
     </div>
   );
 };
