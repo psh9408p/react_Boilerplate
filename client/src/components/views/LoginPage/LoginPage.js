d7c42a8fb7f1b58dee5190c44aa1ff4277b6b2b6
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-09 23:27:20 +0900

    add react-redux signup,login

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+// import Axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
+const LoginPage = (props) => {
+  const dispatch = useDispatch();
 
-const LoginPage = () => {
-  return <div>login</div>;
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onPasswordHanlder = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    let body = {
+      Email,
+      Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push("/");
+      } else {
+        alert("error");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHanlder} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+// import Axios from "axios";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
 
-const RegisterPage = () => {
-  return <div>register</div>;
+const RegisterPage = (props) => {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onPasswordHanlder = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onNameHanlder = (e) => {
+    setName(e.currentTarget.value);
+  };
+  const onConfirmPasswordHanlder = (e) => {
+    setconfirmPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (Password !== confirmPassword) {
+      return alert("password 와 pssword 확인이 같지않습니다.");
+    }
+
+    let body = {
+      Email,
+      Password,
+      name: Name,
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("회갑error");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHanlder} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHanlder} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={onConfirmPasswordHanlder}
+        />
+        <br />
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
